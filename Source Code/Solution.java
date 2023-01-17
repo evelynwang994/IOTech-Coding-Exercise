a7c043add3b705aa45ef14919439680f4e370e4c
evelynwang994 <73518210+evelynwang994@users.noreply.github.com>
2023-01-17 17:17:51 +0000

    Add files via upload
    Add a source code.

---
+++ Source Code/Solution.java
@@ -0,0 +1,107 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Iterator;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+/**
+ * 
+ */
+
+/**
+ * @author Evelyn
+ *
+ */
+public class Solution {
+
+	/**
+	 * @param args
+	 */
+	@SuppressWarnings({ "unchecked", "resource" })
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JSONParser parser = new JSONParser();
+		
+		try {
+			Object obj = parser.parse(new FileReader("exercise-02/data/data.json"));
+			JSONObject jsonObject = (JSONObject) obj;
+			
+			JSONArray ltDevises = (JSONArray) jsonObject.get("Devices");
+			
+			Iterator<JSONObject> iterator = ltDevises.iterator();
+			
+			// Get current time.
+            long curTime = Instant.now().getEpochSecond();
+            
+            // Discard the devices where the timestamp value is before the current time.
+            while (iterator.hasNext()) {
+            	JSONObject device = iterator.next();
+            	String sTimestamp = (String)device.get("timestamp");
+            	long time = Long.parseLong(sTimestamp); // timestamp
+            	
+            	if(time < curTime) {
+            		iterator.remove();
+            	}
+            	
+            }
+            
+            int total = 0;
+            Iterator<JSONObject> secondIt = ltDevises.iterator();
+            
+            int index = 0;
+            String[] arrUUID = new String[ltDevises.size()];
+            
+            while (secondIt.hasNext()) {
+            	// Get the total of all value entries
+            	JSONObject device = secondIt.next();
+            	String value = (String)device.get("value"); // value
+            	
+            	// base64 to integer
+            	byte[] decodedBytes = Base64.getDecoder().decode(value);
+            	
+            	int iValue = 0;
+            	for (byte b : decodedBytes) {
+            		iValue = (iValue << 8) + (b & 0xFF);
+            	}
+            	
+            	total = total + iValue;
+            	
+            	// Parse the uuid from the info field of each entry
+            	String info = (String)device.get("Info"); // info
+            	String[] arrInfo = info.split("\\s+"); // info is split by space
+            	
+            	// UUID
+            	String uuid = arrInfo[3].substring(5, arrInfo[3].length() - 1);
+            	arrUUID[index] = uuid;
+            	
+            	index++;
+            }
+            
+            // Write result to file
+            JSONObject rs = new JSONObject();
+            rs.put("ValueTotal", total);
+            
+            JSONArray ltUUID = new JSONArray();
+            
+            for(int i = 0; i < arrUUID.length; i++) {
+            	ltUUID.add(arrUUID[i]);
+            }
+            rs.put("UUIDS", ltUUID);
+            
+            FileWriter file = new FileWriter("output.json");
+            file.write(rs.toJSONString());
+            
+            file.flush();
+            file.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
